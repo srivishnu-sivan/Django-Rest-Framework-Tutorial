477c662e813f3a222848a40e881cd21838ca223d
srivishnu-sivan <mallikasrivishnu@gmail.com>
2024-03-12 08:06:26 +0530

    basic end point is done

---
+++ py_client/basic.py
@@ -0,0 +1,12 @@
+import requests
+
+
+
+# endpoint = "https://httpbin.org/status/200/"
+endpoint = "http://localhost:8000"
+
+get_response = requests.get(endpoint, json={"query" : "hello world"})
+print(get_response.text)
+print("=================================================================================")
+
+print(get_response.status_code)
