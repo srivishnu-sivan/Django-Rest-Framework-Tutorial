6fc473b511fd3369213521ce0e2ef4bdec97c3ee
srivishnu-sivan <mallikasrivishnu@gmail.com>
2024-03-12 08:31:24 +0530

    creating first project and config it to main project

--- py_client/basic.py
+++ py_client/basic.py
@@ -3,10 +3,10 @@ import requests
 
 
 # endpoint = "https://httpbin.org/status/200/"
-endpoint = "http://localhost:8000"
+endpoint = "http://localhost:8000/api/"
 
 get_response = requests.get(endpoint, json={"query" : "hello world"})
 print(get_response.text)
 print("=================================================================================")
 
-print(get_response.status_code)
+print(get_response.json()['message'])
