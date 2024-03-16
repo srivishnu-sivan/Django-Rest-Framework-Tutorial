b63caf6b97be3efe9680d8cb39e1367fdeae0008
srivishnu-sivan <mallikasrivishnu@gmail.com>
2024-03-16 12:56:51 +0530

    djanog model instance to dict

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,12 +1,15 @@
 from django.http import JsonResponse
 from products.models import Product
+from django.forms.models import model_to_dict
 
 def api_home(request, *args, **kwargs):
     model_data = Product.objects.all().order_by("?").first()
     data = {}
     if model_data:
-        data['id'] = model_data.id
-        data['title'] = model_data.title
-        data['Content'] = model_data.Content
-        data['price'] = model_data.price
+        # data['id'] = model_data.id
+        # data['title'] = model_data.title
+        # data['Content'] = model_data.Content
+        # data['price'] = model_data.price
+        # ! instead of the above lines try the below
+        data = model_to_dict(model_data, fields=['title', 'Content'])
     return JsonResponse(data)
