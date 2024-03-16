ac9d5740858da770724363d525186070e41ea296
srivishnu-sivan <mallikasrivishnu@gmail.com>
2024-03-16 12:06:14 +0530

    Django model instance as api response

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,12 @@
 from django.http import JsonResponse
-
+from products.models import Product
 
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"message" : "Hi there, this is your Django API response!!!"})
+    model_data = Product.objects.all().order_by("?").first()
+    data = {}
+    if model_data:
+        data['id'] = model_data.id
+        data['title'] = model_data.title
+        data['Content'] = model_data.Content
+        data['price'] = model_data.price
+    return JsonResponse(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -5,8 +5,7 @@ import requests
 # endpoint = "https://httpbin.org/status/200/"
 endpoint = "http://localhost:8000/api/"
 
-get_response = requests.get(endpoint, json={"query" : "hello world"})
+get_response = requests.get(endpoint ,json={"product_id" : 123})
 print(get_response.text)
 print("=================================================================================")
 
-print(get_response.json()['message'])
